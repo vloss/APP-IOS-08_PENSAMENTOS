b4d15a241b1760710055a380339d0e1e480562c0
vloss <viniciusvianaloss@gmail.com>
2023-01-28 16:55:20 -0300

    iniciando proejto.

---
+++ Pensamentos/Pensamentos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by Vinicius Loss on 28/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

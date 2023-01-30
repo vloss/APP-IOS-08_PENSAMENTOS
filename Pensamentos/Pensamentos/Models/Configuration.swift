258eb4702d936b20881e673f82d60b63490f7cae
vloss <viniciusvianaloss@gmail.com>
2023-01-30 01:06:00 -0300

    Inicio do uso de UserDefaults

--- Pensamentos/Pensamentos/Models/Configuration.swift
+++ Pensamentos/Pensamentos/Models/Configuration.swift
@@ -7,7 +7,47 @@
 
 import Foundation
 
+enum userDefaultsKeys: String {
+    case timeInterval = "timeInterval"
+    case colorScheme = "colorScheme"
+    case autorefresh = "autorefresh"
+}
+
 class Configuration {
+    
+    let defaults = UserDefaults.standard
+    static var shared: Configuration = Configuration()
+    
+    var timeInterval: Double{
+        get {
+            return defaults.double(forKey: userDefaultsKeys.timeInterval.rawValue)
+        }
+        set{
+            defaults.set(newValue, forKey: userDefaultsKeys.timeInterval.rawValue)
+        }
+    }
+    
+    var colorScheme: Int{
+        get {
+            return defaults.double(forKey: userDefaultsKeys.colorScheme.rawValue)
+        }
+        set{
+            defaults.set(newValue, forKey: userDefaultsKeys.colorScheme.rawValue)
+        }
+    }
+    var timeInterval: Double{
+        get {
+            return defaults.double(forKey: userDefaultsKeys.timeInterval.rawValue)
+        }
+        set{
+            defaults.set(newValue, forKey: userDefaultsKeys.timeInterval.rawValue)
+        }
+    }
+    
+    
+    private init(){
+        
+    }
     
 }
  
